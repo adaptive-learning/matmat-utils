300830712ca8d14ffc9de1531f5f98a824d33f16
Thran <exthran@gmail.com>
2015-03-25 11:57:50 +0100

    more graphs related to speed accuracy trade-off

--- answers.py
+++ answers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def load_answers(csv_file="data/questions_answer.csv"):
@@ -12,13 +13,14 @@ def load_answers(csv_file="data/questions_answer.csv"):
     """
 
     df = pd.DataFrame.from_csv(csv_file)
+    df["log_times"] = np.log(df["solving_time"])
 
     return df
 
 
 def filter_long_times(answers, max_time=100):
     """
-    Remove answers with long and negative solving time
+    Remove answers with long and non-positive solving time
 
     Args:
         answers (pandas.DataFrame)
@@ -27,4 +29,4 @@ def filter_long_times(answers, max_time=100):
         pandas.DataFrame
     """
 
-    return answers[(0 <= answers["solving_time"]) & (answers["solving_time"] < max_time)]
+    return answers[(0 < answers["solving_time"]) & (answers["solving_time"] < max_time)]
--- examples.py
+++ examples.py
@@ -1,18 +1,25 @@
 from answers import *
 from skills import *
 import pylab as plt
-
+import seaborn as sns
+import numpy as np
 
 def response_time_hist(answers=None):
     answers = answers if answers is not None else load_answers()
     answers = filter_long_times(answers)
 
-    answers["solving_time"].hist(bins=50, range=(0, 50))
-    plt.show()
+    sns.set(style="white", palette="muted")
+    sns.kdeplot(answers["solving_time"], shade=True, label="answers {}".format(len(answers)))
+    # sns.kdeplot(answers.groupby("question")["solving_time"].mean(), shade=True, label="questions {}".format(len(answers["question"].unique())))
+    # sns.kdeplot(answers.groupby("user")["solving_time"].mean(), shade=True, label="user {}".format(len(answers["user"].unique())))
+    sns.kdeplot(np.exp(answers.groupby("question")["log_times"].mean()), shade=True, label="questions {}".format(len(answers["question"].unique())))
+    sns.kdeplot(np.exp(answers.groupby("user")["log_times"].mean()), shade=True, label="user {}".format(len(answers["user"].unique())))
+    plt.xlim(0, 30)
 
 
 def time_accuracy_trade_off(answers=None):
     answers = answers if answers is not None else load_answers()
+    answers = filter_long_times(answers)
 
     points = range(0, 51)
     counts = []
@@ -23,6 +30,7 @@ def time_accuracy_trade_off(answers=None):
         counts.append(len(ans))
         success_rate.append(ans["correctly_solved"].mean())
 
+    sns.set(style="white", palette="muted")
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     ax1.plot(points, counts)
@@ -32,6 +40,44 @@ def time_accuracy_trade_off(answers=None):
     ax2.set_ylim([0, 1])
     ax2.set_ylabel("success rate")
 
-    plt.show()
 
-time_accuracy_trade_off()
+def normed_time_accuracy_trade_off(answers=None, normed_according_to="user"):
+    answers = answers if answers is not None else load_answers()
+    answers = filter_long_times(answers)
+    answers = answers.join(np.exp(answers.groupby(normed_according_to)["log_times"].mean()), on=normed_according_to, rsuffix='_mean')
+    answers["normed_time"] = answers["solving_time"] - answers["log_times_mean"]
+
+    points = range(-14, 40)
+    counts = []
+    shifted_success_rate = []
+
+    for i in points:
+        ans = answers[(i <= answers["normed_time"]) & (answers["normed_time"] < i+1)]
+        counts.append(len(ans))
+        shifted_success_rate.append(ans["correctly_solved"].mean())
+
+    plt.title("Normed response time according to log-mean of {}".format(normed_according_to))
+    ax1 = plt.subplot()
+    ax1.plot(points, shifted_success_rate, label="success rate")
+
+    ax1.legend(loc=2)
+    ax2 = ax1.twinx()
+    ax2.plot(points, np.array(counts), color="g", label="number of answers")
+    ax2.legend(loc=1)
+
+
+def log_mean_time_hist(answers=None, normed_according_to="user"):
+    answers = answers if answers is not None else load_answers()
+    answers = filter_long_times(answers)
+    answers = answers.join(np.exp(answers.groupby(normed_according_to)["log_times"].mean()), on=normed_according_to, rsuffix='_mean')
+    plt.title("Histogram of log-means of response time of {}".format(normed_according_to))
+    sns.distplot(answers["log_times_mean"], color="r", label="log-mean times")
+    plt.xlim(0, 30)
+
+response_time_hist()
+# time_accuracy_trade_off()
+# normed_time_accuracy_trade_off()
+# normed_time_accuracy_trade_off(normed_according_to="question")
+# log_mean_time_hist()
+# log_mean_time_hist(normed_according_to="question")
+plt.show()
